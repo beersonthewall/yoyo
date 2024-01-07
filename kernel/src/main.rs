9a9a5c04f778896d7bebbf14b8eab2e525189c24
Matthew <matthew@beersonthewall.com>
2024-01-07 17:38:46 -0600

    Barebones kernel freestanding binary
    Uses an unstable cargo feature (per-package-target) to force the target triple

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[allow(dead_code)]
+fn kmain() -> !{
+    loop {}
 }
+
+#[panic_handler]
+fn panic_handler(_info: &PanicInfo) -> ! {
+    loop {}
+}
+

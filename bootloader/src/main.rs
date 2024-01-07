b2c5388e5a7232098dc32f3b22f24ba9d95caa4b
Matthew <matthew@beersonthewall.com>
2024-01-07 17:26:36 -0600

    Barebones UEFI setup for bootloader.
    One note: to build the bootloader for x86_64-unknown-uefi I had to use
    an unstable cargo feature (per-package-target). This is because cargo,
    when invoked from the workspace root, would ignore a member's cargo config file with
    build.target information in it.

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+use log::info;
+use uefi::{
+    prelude::*,
+    table::boot::MemoryType,
+};
+
+#[entry]
+fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    // At this point uefi has done all the usual platform init, plus it has landed us
+    // in long-mode with identity mapped pages.
+    uefi_services::init(&mut system_table).unwrap();
+    info!("memory::init()");
+    info!("exit_boot_services()");
+    let (mut _system_table, mut _memory_map) = system_table.exit_boot_services(MemoryType::CONVENTIONAL);
+    Status::SUCCESS
 }

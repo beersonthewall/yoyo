bc0af8a96fa213eb4c09398262e12d761825887e
Matthew <matthew@beersonthewall.com>
2024-01-07 19:26:14 -0600

    Simplifying changes to the bootloader barebones.

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,18 +2,14 @@
 #![no_std]
 
 use log::info;
-use uefi::{
-    prelude::*,
-    table::boot::MemoryType,
-};
+use uefi::prelude::*;
 
 #[entry]
 fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     // At this point uefi has done all the usual platform init, plus it has landed us
     // in long-mode with identity mapped pages.
     uefi_services::init(&mut system_table).unwrap();
-    info!("memory::init()");
-    info!("exit_boot_services()");
-    let (mut _system_table, mut _memory_map) = system_table.exit_boot_services(MemoryType::CONVENTIONAL);
+    info!("Hello, UEFI!");
+    system_table.boot_services().stall(10_000_000);
     Status::SUCCESS
 }

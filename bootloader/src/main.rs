9213b5eff313d98bf8ad669823cf8a3c55230e95
Matthew <matthew@beersonthewall.com>
2024-03-04 21:52:40 +0000

    Bootloader: read kernel binary from boot partition

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,31 +2,41 @@
 #![no_std]
 
 use log::info;
-use uefi::{prelude::*, table::boot::OpenProtocolParams, proto::device_path::DevicePath, Result};
+use uefi::{
+    Result,
+    prelude::*,
+    proto::media::file::{
+	FileAttribute,
+	FileMode,
+	File,
+    },
+    data_types::CStr16,
+};
+
+const KERNEL_PATH: &'static str = "\\efi\\boot\\kernel";
 
 fn load_kernel(image_handle: Handle, boot_services: &BootServices) -> Result<Status> {
-    let device_path_proto_handle = boot_services.get_handle_for_protocol::<DevicePath>()
-	.expect("to have the device path protocol handle");
-    let protocol = boot_services.open_protocol_exclusive::<DevicePath>(device_path_proto_handle)?;
+    let mut simple_fs_proto = boot_services.get_image_file_system(image_handle)?;
+    let mut root_dir = simple_fs_proto.open_volume()?;
+    let mut buf = [0;64];
+    let kernel = root_dir.open(CStr16::from_str_with_buf(KERNEL_PATH, &mut buf).unwrap(), FileMode::Read, FileAttribute::empty())?;
+    info!("Hello, uefi!");
+    boot_services.stall(10_000_000);
     Ok(Status::SUCCESS)
 }
 
+/// UEFI Entrypoint.
+///
+/// Assumes the kernel binary is a sibiling file to the uefi os loader. The EFI boot partition
+/// filesystem should look like this:
+///
+/// efi
+/// |- boot/
+///    |- BOOTx64.efi
+///    |- kernel
 #[entry]
 fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
-    // At this point uefi has done all the usual platform init, plus it has landed us
-    // in long-mode with identity mapped pages.
-
-    /*
-    What actually needs to happen?
-    - Setup page tables
-    - load kernel binary into memory
-    - call exit boot services
-    - start executing the kernel
-     */
-
     uefi_services::init(&mut system_table).unwrap();
-    info!("Hello, uefi!");
     let boot_services = system_table.boot_services();
-    boot_services.stall(10_000_000);
     load_kernel(image_handle, boot_services).status()
 }

03396b0cae724c9869ce9206990e83e77d524e69
Matthew <matthew@beersonthewall.com>
2024-03-07 20:46:37 +0000

    Bootloader: better way to get file size

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -9,7 +9,7 @@ use uefi::{
 	FileAttribute,
 	FileMode,
 	File,
-	RegularFile,
+	FileInfo,
     },
     
     data_types::CStr16,
@@ -27,13 +27,14 @@ fn load_kernel(image_handle: Handle, boot_services: &BootServices) -> Result<Sta
     let mut root_dir = simple_fs_proto.open_volume()?;
     let mut buf = [0;64];
     let kernel = root_dir.open(CStr16::from_str_with_buf(KERNEL_PATH, &mut buf).unwrap(), FileMode::Read, FileAttribute::empty())?;
+
     info!("Hello, uefi!");
     info!("Parsing kernel elf binary...");
 
     let mut kernel = kernel.into_regular_file().expect("The kernel to be a regular file");
-    kernel.set_position(0).expect("Set position");
-    kernel.set_position(RegularFile::END_OF_FILE).expect("Set position to end");
-    let kernel_sz = kernel.get_position().expect("kernel size") as usize;
+    let mut buf = [0;500];
+    let file_info: &mut FileInfo = kernel.get_info(&mut buf).expect("file info");
+    let kernel_sz = usize::try_from(file_info.file_size()).unwrap();
 
     let kbuf = boot_services.allocate_pool(MemoryType::RESERVED, kernel_sz).expect("kernel buf alloc");
     let kbuf = unsafe { core::slice::from_raw_parts_mut(kbuf, kernel_sz) };
